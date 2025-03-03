6b9ac481f19b45cbac22d28d30fdd23fd86b9f52
Tarik Merrylees <tarikmerrylees@gmail.com>
2025-03-03 16:46:58 +0000

    correct grammar

--- src/routes/theory/+page.svelte
+++ src/routes/theory/+page.svelte
@@ -5,8 +5,7 @@
 <h1>Aurebesh Essentials</h1>
 
 <p >
-  Aurebesh is the "basic" (English) alphabet in the Star Wars universe. It is used to write the Galactic Basic Standard language. The alphabet is based on English, but the letters are changed to look like the Star Wars universe. The alphabet is used in the Star Wars movies and is used in the Star Wars expanded universe. In the new canon, digraphs have begun to be introduced. The font used on this site has digraphs, however, much of canon still hasn;t adapted it. So you will see a discrepency of its use. You can refer to the <a href="https://starwars.fandom.com/wiki/Aurebesh" target="_blank">Wookieepedia</a> for more information. The table below will always be accessible to you whenever you need to a aurebesh to roman alphabet reference.
-</p>
+  Aurebesh is the "basic" (English) alphabet in the Star Wars universe. It is used to write the Galactic Basic Standard language. The alphabet is based on English, but the letters are changed to look futuristic. The alphabet is used in the Star Wars movies and in the Star Wars expanded universe. The font used on this site has digraphs, however, much of canon still hasn’t adapted it. So you may see a discrepancy of its use. For example, many of the video games may opt to avoid using digraphs at times. You can refer to the <a href="https://starwars.fandom.com/wiki/Aurebesh" target="_blank">Wookieepedia</a> for more information. The table below will always be accessible to you whenever you need to a aurebesh to roman alphabet reference. Simply click on the “help” button on the center of the header above to toggle it on and off.</p>
 
 <div class="table">
   <AurebeshTable />
